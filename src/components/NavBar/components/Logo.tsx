052caf445ca628c1c9bbea13f3754404efe71e3e
ethanpham1811 <khoi.drca@gmail.com>
2023-12-21 13:37:08 +0700

    feat: add logo, change to next Image

--- src/components/NavBar/components/Logo.tsx
+++ src/components/NavBar/components/Logo.tsx
@@ -1,10 +1,12 @@
+import logo from '@/assets/images/logo.png'
+import Image from 'next/image'
 import Link from 'next/link'
 
 function Logo() {
   return (
     <div className="flex flex-shrink-0 items-center">
       <Link href="/">
-        <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" />
+        <Image width={40} height={40} src={logo} alt="Vacation Buddy Logo" />
       </Link>
     </div>
   )
