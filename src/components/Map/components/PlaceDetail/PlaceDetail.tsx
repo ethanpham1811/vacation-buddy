5bd5e8e3df4b9caaa55aa370edebe57a9e64a26d
ethanpham1811 <khoi.drca@gmail.com>
2023-12-27 01:11:50 +0700

    feat: implement place detail (wip)

--- src/components/Map/components/PlaceDetail/PlaceDetail.tsx
+++ src/components/Map/components/PlaceDetail/PlaceDetail.tsx
@@ -1,4 +1,7 @@
+import { Spacer } from '@/components'
 import { TPlaceInfo } from '@/constants/types'
+import Photo from './components/Photo'
+import RatingAddress from './components/RatingAddress'
 
 type TPlaceDetailProps = {
   place: TPlaceInfo
@@ -7,7 +10,21 @@ type TPlaceDetailProps = {
 function PlaceDetail({ place }: TPlaceDetailProps) {
   const { id, photo, name, web_url, website, address, description, phone, rating, num_reviews, open_now_text, price } = place
 
-  return <div className="flex flex-col"></div>
+  return (
+    <div className="flex flex-col">
+      {/* photo with favorite btn*/}
+      <Photo photo={photo} name={name} />
+
+      <div className="p-4 flex flex-col gap-1 shadow-card">
+        {/* title */}
+        <h1 className="font-semibold text-lg">{name}</h1>
+        <Spacer />
+
+        {/* rating & address */}
+        {(address || rating) && <RatingAddress rating={rating} num_reviews={num_reviews} address={address} />}
+      </div>
+    </div>
+  )
 }
 
 export default PlaceDetail
---
+++ src/components/Map/components/PlaceDetail/components/Photo.tsx
@@ -0,0 +1,23 @@
+import NoImgPlaceholder from '@/components/utilities/NoImgPlaceholder'
+import { BLURRED_DATA_URL } from '@/constants/enum'
+import { IoHeart } from '@/constants/icons'
+import Image from 'next/image'
+
+function Photo({ photo, name }: { photo: string | undefined; name: string }) {
+  return (
+    <div className="relative h-60 flex items-stretch relative">
+      {photo ? (
+        <Image layout="fill" style={{ objectFit: 'cover' }} placeholder="blur" blurDataURL={BLURRED_DATA_URL} alt={`photo of ${name}`} src={photo} />
+      ) : (
+        <NoImgPlaceholder />
+      )}
+
+      {/* favorite */}
+      <div className="absolute right-7 bottom-0 translate-y-1/2 shadow-card text-red-400 p-3 rounded-full bg-white cursor-pointer hover:bg-red-400 hover:text-white">
+        <IoHeart size={25} />
+      </div>
+    </div>
+  )
+}
+
+export default Photo
