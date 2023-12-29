625b24be716d1dcef3282b8f90eca0f9c048ddad
ethanpham1811 <khoi.drca@gmail.com>
2023-12-30 00:25:11 +0700

    feat: add Vacation buddy logo, remove logo link

--- src/components/NavBar/components/Logo.tsx
+++ src/components/NavBar/components/Logo.tsx
@@ -1,14 +1,12 @@
 'use client'
 import logo from '@/assets/images/logo.png'
 import Image from 'next/image'
-import Link from 'next/link'
 
 function Logo() {
   return (
-    <div className="flex flex-shrink-0 items-center">
-      <Link href="/">
-        <Image priority width={40} height={40} src={logo} alt="Vacation Buddy Logo" />
-      </Link>
+    <div className="flex flex-shrink-0 items-center gap-4">
+      <Image priority width={40} height={40} src={logo} alt="Vacation Buddy Logo" />
+      <div className="text-white font-bold hidden lg:block">VACATION BUDDY</div>
     </div>
   )
 }
