cc3b04c4addc7a3fc405ccce389dd70295442922
ethanpham1811 <khoi.drca@gmail.com>
2023-12-29 23:08:43 +0700

    feat: handle locate pin on clicking saved location

--- src/components/NavBar/components/HistoryToggler/HistoryList.tsx
+++ src/components/NavBar/components/HistoryToggler/HistoryList.tsx
@@ -1,22 +1,44 @@
 import { XMarkIcon } from '@/constants/icons'
-import { TFavorite } from '@/constants/types'
+import { setActivePoint } from '@/lib/features/activePoint/activePointSlice'
+import { removeFavorite } from '@/lib/features/favoriteList/favoriteListSlice'
+import { useAppDispatch, useAppSelector } from '@/lib/hooks'
+import { useQueryState } from 'next-usequerystate'
 import Image from 'next/image'
+import { Dispatch, SetStateAction } from 'react'
 import LinesEllipsis from 'react-lines-ellipsis'
 
 type THistoryListProps = {
-  data: TFavorite[]
-  removeFavorite: (id: string) => void
-  locatePlace: ({ id, lat, lng }: { id: string; lat: number; lng: number }) => void
+  setOpen: Dispatch<SetStateAction<boolean>>
 }
 
-function HistoryList({ data, removeFavorite, locatePlace }: THistoryListProps) {
+function HistoryList({ setOpen }: THistoryListProps) {
+  const [_paramLat, setParamLat] = useQueryState('lat')
+  const [_paramLng, setParamLng] = useQueryState('lng')
+  const dispatch = useAppDispatch()
+  const favoriteList = useAppSelector((state) => state.favoriteList.data)
+
+  function handleLocatePlace({ id, lat, lng }: { id: string; lat: number; lng: number }) {
+    // set lat lng search params => trigger map move & data fetching
+    setParamLat(lat.toString())
+    setParamLng(lng.toString())
+
+    // set active point
+    dispatch(setActivePoint({ id, lat, lng }))
+
+    // close drawer
+    setOpen(false)
+  }
+  function handleRemoveFavorite(id: string) {
+    dispatch(removeFavorite(id))
+  }
+
   return (
     <div className="grid gap-5 grid-cols-2">
-      {data.map(({ id, name, photo, lat, lng }) => (
+      {favoriteList.map(({ id, name, photo, lat, lng }) => (
         <div
           key={`favorite_${id}`}
           className="text-xs p-1 shadow-card flex items-center overflow-hidden cursor-pointer bg-white rounded-md min-h-[50px] hover:bg-gray-800 hover:text-white"
-          onClick={() => locatePlace({ id, lat, lng })}
+          onClick={() => handleLocatePlace({ id, lat, lng })}
         >
           {/* photo */}
           <div className="px-2">
@@ -29,8 +51,14 @@ function HistoryList({ data, removeFavorite, locatePlace }: THistoryListProps) {
           </div>
 
           {/* close btn */}
-          <div className="cursor-pointer transition-all self-stretch rounded-md bg-gray-600 hover:bg-gray-800 text-white flex items-center px-1">
-            <XMarkIcon className="h-4 w-4 font-semibold" onClick={() => removeFavorite(id)} />
+          <div
+            onClick={(e) => {
+              e.stopPropagation()
+              handleRemoveFavorite(id)
+            }}
+            className="remove-btn cursor-pointer transition-all self-stretch rounded-md bg-gray-600 hover:bg-red-500 text-white flex items-center px-1"
+          >
+            <XMarkIcon className="h-4 w-4 font-semibold" />
           </div>
         </div>
       ))}
--- src/components/NavBar/components/HistoryToggler/HistoryToggler.tsx
+++ src/components/NavBar/components/HistoryToggler/HistoryToggler.tsx
@@ -1,28 +1,17 @@
 'use client'
 import { Drawer, Toggler } from '@/components'
 import { IoHeart } from '@/constants/icons'
-import { removeFavorite } from '@/lib/features/favoriteList/favoriteListSlice'
-import { useAppDispatch, useAppSelector } from '@/lib/hooks'
 import { useState } from 'react'
 import HistoryList from './HistoryList'
 
 function HistoryToggler() {
   const [open, setOpen] = useState(false)
-  const dispatch = useAppDispatch()
-  const favoriteList = useAppSelector((state) => state.favoriteList.data)
-
-  function handleLocatePlace({ id, lat, lng }: { id: string; lat: number; lng: number }) {
-    // dispatch(removeFavorite(id))
-  }
-  function handleRemoveFavorite(id: string) {
-    dispatch(removeFavorite(id))
-  }
 
   return (
     <div>
       <Toggler onClick={() => setOpen(true)} title="history" icon={<IoHeart className="h-6 w-6 text-red-500" />} />
       <Drawer title="Your saved locations" open={open} setOpen={setOpen}>
-        <HistoryList data={favoriteList} removeFavorite={handleRemoveFavorite} locatePlace={handleLocatePlace} />
+        <HistoryList setOpen={setOpen} />
       </Drawer>
     </div>
   )
