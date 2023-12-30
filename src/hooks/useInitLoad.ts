6abdc7863215cf5f670ec1872893100fcb0a0e09
ethanpham1811 <khoi.drca@gmail.com>
2023-12-30 13:36:29 +0700

    feat: add type search param (attraction by default) on initial load

--- src/hooks/useInitLoad.ts
+++ src/hooks/useInitLoad.ts
@@ -1,17 +1,21 @@
-import { DEFAULT_ZOOM } from '@/constants/enum'
+import { API_TYPES, DEFAULT_ZOOM } from '@/constants/enum'
 import { Map } from 'leaflet'
 import { useQueryState } from 'next-usequerystate'
 import { useEffect } from 'react'
 
 /**
- * on initial load: move map to current user's location if no lat & lng search params
+ * on initial load, if no search params provided:
+ * - move map to current user's location
+ * - update lat and lng (user's location), type (attraction by default)
  */
 function useInitLoad(map: Map) {
   const [lat] = useQueryState('lat')
   const [lng] = useQueryState('lng')
+  const [type, setType] = useQueryState('type')
 
   useEffect(() => {
     !lat && !lng && map && map.locate({ setView: true, maxZoom: DEFAULT_ZOOM })
+    !type && setType(API_TYPES.attractions)
   }, [map, lat, lng])
 }
 
