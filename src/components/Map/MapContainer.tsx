1140c847ea6af7918648a9346c4a0550aaa3f872
ethanpham1811 <khoi.drca@gmail.com>
2023-12-31 11:12:43 +0700

    feat: add minzoom

--- src/components/Map/MapContainer.tsx
+++ src/components/Map/MapContainer.tsx
@@ -1,13 +1,13 @@
 'use client'
-import { DEFAULT_ZOOM } from '@/constants/enum'
+import { DEFAULT_ZOOM, MIN_ZOOM } from '@/constants/enum'
 import 'leaflet/dist/leaflet.css'
 import { MapContainer as LeafletProvider } from 'react-leaflet/MapContainer'
 import Map from './components/Map/Map'
 
 function MapContainer() {
   return (
-    <div className="h-full flex flex-1 justify-center items-center">
-      <LeafletProvider center={[33.837694, 106.8318812]} zoom={DEFAULT_ZOOM} style={{ width: '100%', height: '100%' }}>
+    <div className="flex h-full flex-1 items-center justify-center">
+      <LeafletProvider center={[12.91285, 100.87808]} minZoom={MIN_ZOOM} zoom={DEFAULT_ZOOM} style={{ width: '100%', height: '100%' }}>
         <Map />
       </LeafletProvider>
     </div>
--- src/constants/enum.ts
+++ src/constants/enum.ts
@@ -3,7 +3,7 @@ export const DEBOUNCE_TIMER_AUTOCOMPLETE = 300
 export const DEBOUNCE_TIMER_MOVE_VIEWPORT = 500
 export const MAPBOX_MAP_GL_STYLE = 'mapbox://styles/mapbox/streets-v9'
 export const QUERY_LIMIT = 10
-export const DEFAULT_ZOOM = 14
+export const DEFAULT_ZOOM = 13
 export const MIN_ZOOM = 13
 export const FAVORITE_COOKIE = 'vacation-buddy-favorite-list'
 
